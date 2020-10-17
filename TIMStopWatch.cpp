021b21e4c3c2500df63556be2f52c73890177cdc
SatoshiOhya <conatus.11099@gmail.com>
2020-10-17 16:24:11 +0900

    動作確認できた

--- TIMStopWatch.cpp
+++ TIMStopWatch.cpp
@@ -6,3 +6,38 @@
  */
 
 #include "TIMStopWatch.h"
+
+void TIM_StopWatch::start(){
+	update();
+	HAL_TIM_Base_Start(htim);
+	isStart = true;
+}
+
+void TIM_StopWatch::stop(){
+	isStart = false;
+	HAL_TIM_Base_Stop(htim);
+}
+
+void TIM_StopWatch::reset(){
+	stop();
+	count = 0;
+	__HAL_TIM_SET_COUNTER(htim, 0);
+	__HAL_TIM_CLEAR_FLAG(htim, TIM_FLAG_UPDATE);
+}
+
+void TIM_StopWatch::update(){
+	uint32_t cnt = __HAL_TIM_GET_COUNTER(htim);
+
+	if (__HAL_TIM_GET_FLAG(htim, TIM_FLAG_UPDATE)) {//over_flow
+		count += cnt - (befCount - __HAL_TIM_GET_AUTORELOAD(htim));
+		__HAL_TIM_CLEAR_FLAG(htim, TIM_FLAG_UPDATE);
+	} else {
+		count += cnt - befCount;
+	}
+
+	befCount = cnt;
+}
+
+float TIM_StopWatch::getTimeMS(){
+	return count / (float)frequence * 1000.0f;
+}
--- TIMStopWatch.h
+++ TIMStopWatch.h
@@ -8,9 +8,28 @@
 #ifndef SRC_TIMSTOPWATCH_H_
 #define SRC_TIMSTOPWATCH_H_
 
+#include "main.h"
+
 class TIM_StopWatch {
 public:
-	TIM_StopWatch();
+	TIM_StopWatch(TIM_HandleTypeDef *htim=nullptr, uint32_t frequence = 1000000)
+	:htim(htim),
+	 frequence(frequence)
+	{
+	}
+
+	void start();
+	void stop();
+	void reset();
+	float getTimeMS();
+	void update();
+
+private:
+	TIM_HandleTypeDef *htim;
+	const uint32_t frequence;
+	uint32_t count = 0;
+	uint32_t befCount = 0;
+	bool isStart = false;
 };
 
 #endif /* SRC_TIMSTOPWATCH_H_ */
