11493347aa727e1fc497a2c8291aeb6ada223429
OHYA Satoshi <44540173+SatoshiRC@users.noreply.github.com>
2024-07-03 09:56:15 +0900

    add brief comments on each functions

--- elapsedTimer.h
+++ elapsedTimer.h
@@ -19,11 +19,39 @@ public:
 	{
 	}
 
+	/*
+	 * Start or restart the timer.
+	 * This function does not discard the previous number of millisecond elapsed
+	 * Please use reset() before start() to make sure that the initial number of millisecond elapsed is 0
+	 */
 	void start();
+
+	/*
+	 * Stop the timer.
+	 */
 	void stop();
+
+	/*
+	 * Stop the timer and discard the number of  millisecond elapsed.
+	 */
 	void reset();
+
+	/*
+	 * Return the number of millisecond elapsed since previous start().
+	 */
 	float getTimeMS();
+
+	/*
+	 * This function dose not have return value.
+	 * Update internally the number of millisecond elapsed since previous start()
+	 */
 	void update();
+
+	/*
+	 * Test if the configurations are correct.
+	 * Return true when the configurations are correct.
+	 * Return false when the configurations are incorrect.
+	 */
 	bool selfTest();
 
 private:
